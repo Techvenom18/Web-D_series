2075f872771a984ac6ce635873c89e1e0a2232cc
Sumit Rathore <sumitrathore45528@gmail.com>
2025-11-10 01:55:02 +0530

    Plain-Function and Arrow-Function.js
    Demonstrates the use of functions and loops. It defines an arrow function getExp(x, y) that calculates and returns the exponent value of x raised to y. Then it prints numbers from 1 to 15 using three types of loops  a for loop, a while loop, and a do-while loop  showing different ways to iterate through numbers.

---
+++ Javascript/43_plainFunction&ArrowFunction.js
@@ -0,0 +1,26 @@
+// Arrow Functions:- 
+// doo practice 
+
+let getExp = (x,y) => {
+    let ans = x**y;
+    return ans;
+}
+
+console.log(getExp(2,10));
+
+
+for(i=1; i<=15; i++){
+    console.log(i);
+}
+
+let a = 1;
+while(a<=15){
+    console.log(a)
+    a++;
+}
+
+let b=0;
+do{
+    console.log(b)
+    b++;
+} while(b<=15)
